72d39903e4caed89c481427bd1e53d7ec59b07fb
Daniel Gruno <humbedooh@apache.org>
2020-08-11 23:34:03 +0200

    add --fof and disregard args in yaml specs
    --fof (fail on failure) stops the suite if it hits a single error.
    Useful for quick debugging

--- runall.py
+++ runall.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
                         help="Root directory of Apache Pony Mail")
     parser.add_argument('--load', dest='load', type=str, nargs='+',
                         help="Load only specific yaml spec files instead of all test specs")
+    parser.add_argument('--fof', dest='failonfail', action='store_true',
+                        help="Stop running more tests if an error is encountered")
     args = parser.parse_args()
 
     if args.load:
@@ -25,10 +27,13 @@ if __name__ == '__main__':
     tests_total = 0
     now = time.time()
 
+    failbreak = False
     for spec_file in spec_files:
         with open(spec_file, 'r') as f:
             yml = yaml.safe_load(f)
             for test_type in yml:
+                if test_type == 'args':
+                    continue
                 tests_total += 1
                 print("Running '%s' tests from %s..." % (test_type, spec_file))
                 try:
@@ -37,6 +42,11 @@ if __name__ == '__main__':
                 except subprocess.CalledProcessError as e:
                     print("%s test from %s failed with code %d" % (test_type, spec_file, e.returncode))
                     tests_failure += 1
+                    if args.failonfail:
+                        failbreak = True
+                        break
+        if failbreak:
+            break
 
     print("-------------------------------------")
     print("Done with %u tests in %.2f seconds" % (tests_total, time.time() - now))
