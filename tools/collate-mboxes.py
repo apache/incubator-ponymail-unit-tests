90004871405a69fb8e93d4ef210249154214f039
Daniel Gruno <humbedooh@apache.org>
2020-08-24 14:00:59 +0200

    Add "multi-import test" and corpus

---
+++ tools/collate-mboxes.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Simple tool for collating multiple mbox files into a single one, sorted by message ID.
+Used for multi-import tests where you wish to check that multiple sources give the same ID
+"""
+import mailbox
+import sys
+
+outmbox = sys.argv[1]
+msgfiles = sys.argv[2:]
+
+allmessages = {}
+for msgfile in msgfiles:
+    messages = mailbox.mbox(
+        msgfile, None, create=False
+    )
+    for key in messages.iterkeys():
+        message = messages.get(key)
+        message_raw = messages.get_bytes(key)
+        msgid = message.get('message-id')
+        if msgid:
+            msgid = msgid.strip()
+            allmessages[msgid] = message_raw
+
+
+nw = 0
+with open(outmbox, "wb") as f:
+    for key in sorted(allmessages.keys()):
+        f.write(b"From TEST@TEST\r\n")
+        f.write(allmessages[key])
+        f.write(b"\r\n")
+        nw += 1
+
+print("Wrote %u emails to %s" % (nw, outmbox))
