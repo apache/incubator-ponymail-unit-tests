d67be3dd588eadd05bdbecb85d039691200c084a
Sebb <sebb@apache.org>
2020-08-29 15:07:15 +0100

    Bug: collate-output.py should not use get_bytes
    ... and should preserve From line #4
    Reports (but drops) mails with no Message-Id

--- tools/collate-mboxes.py
+++ tools/collate-mboxes.py
@@ -3,35 +3,46 @@
 Simple tool for collating multiple mbox files into a single one, sorted by message ID.
 Used for multi-import tests where you wish to check that multiple sources give the same ID
 
-WARNING: emails without a Message-ID are currently silently dropped
-The code also assumes that mboxes have CRLF line-endings
+WARNING: emails without a Message-ID are currently dropped
 """
 import mailbox
 import sys
 
 outmbox = sys.argv[1]
-msgfiles = sys.argv[2:]
+msgfiles = sys.argv[2:] # multiple input files allowed
 
 allmessages = {}
+noid = 0
 for msgfile in msgfiles:
     messages = mailbox.mbox(
         msgfile, None, create=False
     )
     for key in messages.iterkeys():
         message = messages.get(key)
-        message_raw = messages.get_bytes(key)
         msgid = message.get('message-id')
         if msgid:
             msgid = msgid.strip()
-            allmessages[msgid] = message_raw
+            allmessages[msgid] = key
+        else:
+            print("No message id: ", message.get_from())
+            noid += 1
 
 
 nw = 0
+crlf = None # assume that all emails have the same EOL
 with open(outmbox, "wb") as f:
     for key in sorted(allmessages.keys()):
-        f.write(b"From TEST@TEST\r\n")
-        f.write(allmessages[key])
-        f.write(b"\r\n")
+        file=messages.get_file(allmessages[key], True)
+        if crlf is None:
+            from_ = file.readline()
+            f.write(from_)
+            crlf = (from_.endswith(b'\r\n'))
+        f.write(file.read())
+        if crlf:
+            f.write(b'\r\n')
+        else:
+            f.write(b'\n')
+        file.close()
         nw += 1
 
-print("Wrote %u emails to %s" % (nw, outmbox))
+print("Wrote %u emails to %s with CRLF %s (%u skipped)" % (nw, outmbox, crlf, noid))
